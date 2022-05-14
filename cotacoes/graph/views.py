1e559dcc7c52fde8b3f7aacb516bbfb60ddad760
MacSoares <macario.junior@gmail.com>
2022-05-14 11:05:00 -0300

    Changing chart to the root of 0.0.0.1:8000

--- cotacoes/graph/views.py
+++ cotacoes/graph/views.py
@@ -5,23 +5,16 @@ from graph.models import Currency
 
 def home(request):
     dataset = Currency.objects.all().order_by('base_date')
-    # currency = {}
-    # for line in dataset:
-    #     currency[f"{str(line.base_date.date())}"] = {
-    #         "eur": str(line.eur),
-    #         "brl": str(line.brl),
-    #         "jpy": str(line.jpy),
-    #     }
-
     base_date = list()
     eur = list()
     brl = list()
     jpy = list()
     for line in dataset:
-        base_date.append(line.base_date.date())
-        eur.append(float(line.eur))
-        brl.append(float(line.brl))
-        jpy.append(float(line.jpy))
+        if not line.base_date.date() == base_date[:-1]:
+            base_date.append(line.base_date.date())
+            eur.append(float(line.eur))
+            brl.append(float(line.brl))
+            jpy.append(float(line.jpy))
 
     data = {"base_date": base_date, "eur": eur, "brl": brl, "jpy": jpy}
     return render(request, 'home.html', data)
