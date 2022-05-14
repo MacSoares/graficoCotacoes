c03a8e58ed0a01a0df669f3cc35746def444a4dd
MacSoares <macario.junior@gmail.com>
2022-05-14 08:59:00 -0300

    Starting database migrations

--- cotacoes/graph/models.py
+++ cotacoes/graph/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Currency(models.Model):
+    base_date = models.DateTimeField()
+    eur = models.DecimalField(max_digits=6, decimal_places=4)
+    usd = models.DecimalField(max_digits=6, decimal_places=4)
+    jpy = models.DecimalField(max_digits=6, decimal_places=4)
